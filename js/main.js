bb85dfdca03e16bfa453d1a10fc2f105322d086d
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-06-03 14:55:22 -0700

    Slider basic functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $('.my-slider').unslider({
       nav: false,
+      arrows: {
+      	prev: '<div class="arrow-div div-prev "><a class="unslider-arrow prev"></a></div>',
+      	next: '<div class="arrow-div div-next"><a class="unslider-arrow next"></a></div>',
+      }
     });
   });
