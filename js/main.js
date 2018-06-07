744f6f5c806d965bdb9d192305583d06f4707926
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-06-06 22:24:55 -0700

    Title screen animation and comments added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // changes to default unslider plugin arrows
     $('.my-slider').unslider({
       nav: false,
       arrows: {
@@ -6,4 +8,20 @@ $(document).ready(function() {
       	next: '<div class="arrow-div div-next"><a class="unslider-arrow next"></a></div>',
       }
     });
+
+    // title screen animation
+    let tl = new TimelineMax({repeat: -1, repeatDelay: 1});
+    let pinkAnim = {fill:"#ff00cf", repeat: 1, yoyo: true,  ease: Power4.easeOut};
+    let greyAnim = {fill:"#d3d3d3", repeat: 1, yoyo: true,  ease: Power4.easeOut};
+    let animLength = 0.75;
+
+    tl.to("#A-3", animLength, pinkAnim, 0)
+      .to("#R", animLength, pinkAnim, 1.0)
+      .to("#T", animLength, pinkAnim, 2.0 )
+      .to("#A", animLength, greyAnim, 5.0)
+      .to("#R-2", animLength, greyAnim, 6.0)
+      .to("#T", animLength, greyAnim, 7.0)
+      .to("#A-2", animLength, pinkAnim, 10.0)
+      .to("#R", animLength, pinkAnim, 11.0)
+      .to("#T", animLength, pinkAnim, 12.0)
   });
